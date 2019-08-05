6414e6d5eb0b807090eef52a5cb927376fc53f9e
Alex Basson <alexbasson@gmail.com>
2019-08-04 21:25:15 -0500

    Add delay to see loading state

--- src/OrderService.js
+++ src/OrderService.js
@@ -3,6 +3,13 @@ export default class OrderService {
   getOrdersForCustomer(firstName) {
     return fetch("fixtures/orders.json")
       .then(response => response.json())
+      .then((data) => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(data);
+          }, 1000);
+        });
+      })
       .then(data => {
         if (!data.hasOwnProperty(firstName.toLowerCase())) {
           throw new Error(`no customer with first name ${firstName}`);
